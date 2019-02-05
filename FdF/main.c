6f96d9b429ed0e11204b397a7c93569c79020692
Remi FUMERON <rfumeron@e1r5p4.42.fr>
2019-02-05 16:49:36 +0100

    init

--- FdF/main.c
+++ FdF/main.c
@@ -6,7 +6,7 @@
 /*   By: rfumeron <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 16:15:56 by rfumeron          #+#    #+#             */
-/*   Updated: 2019/01/15 12:07:40 by rfumeron         ###   ########.fr       */
+/*   Updated: 2019/02/05 10:39:26 by rfumeron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ int	main(int ac, char **av)
 {
 	int		fd;
 	char	***points;
-	int		x;
-	int		y;
+//	int		x;
+//	int		y;
 
 	if (ac != 2 || !check_file_name(av[1]))
 	{
@@ -41,20 +41,20 @@ int	main(int ac, char **av)
 		return (-1);
 	}
 	draw(points);
-	x = 0;
-	while (points[x])
-	{
-		y = 0;
-		while (points[x][y])
-		{
-			ft_putstr("x: :");
-			ft_putnbr((float)x * 82);
-			ft_putstr("  y: ");
-			ft_putnbr((float)y * 82);
-			ft_putstr("  z: ");
-			ft_putnbr_endl((float)(ft_atoi(points[x][y++])) * 82);
-		}
-		x++;
-	}
+//	x = 0;
+//	while (points[x])
+//	{
+//		y = 0;
+//		while (points[x][y])
+//		{
+//			ft_putstr("x: :");
+//			ft_putnbr((float)x * 82);
+//			ft_putstr("  y: ");
+//			ft_putnbr((float)y * 82);
+//			ft_putstr("  z: ");
+//			ft_putnbr_endl((float)(ft_atoi(points[x][y++])) * 82);
+//		}
+//		x++;
+//	}
 	return (0);
 }
