502318bacfc159670cf6a5ef2362f56c778074a8
Patatozor <patatozor@MacBook-Air-de-Patatozor.local>
2019-04-03 00:54:00 +0200

    ssh config

--- vmrepo/create_user.sh
+++ vmrepo/create_user.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ssh root:root@10.13.14.1
 adduser default_user
 sudo adduser default_user
 usermod -aG sudo default_user
--- vmrepo/ssh_setup2.sh
+++ vmrepo/ssh_setup2.sh
@@ -2,6 +2,7 @@
 echo "Type yes then ENTER when asked"
 cat ~/.ssh.id_rsa.pub | ssh root@10.13.14.1 -p 9999 "mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys && chmod -R go = ~/.ssh && cat >> ~/.ssh/authorized_keys"
 chown -R default_user:default ~/.ssh
+sed -i '/^Port/s/22/9999/' /etc/ssh/sshd_config
 sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
 sed -i '/^PermitAuthentication/s/yes/no/' /etc/ssh/sshd_config
 systemctl restart ssh
